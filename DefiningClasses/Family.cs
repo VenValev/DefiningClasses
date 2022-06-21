2c1eeebb776a948223b603d6abe09a3cb10c319d
Vencislav <vencislav67@hotmail.com>
2022-06-21 18:49:06 +0300

    3

---
+++ DefiningClasses/Family.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        //Fields
+        private List<Person> familyMembers;
+
+        //Constructor
+
+        public Family()
+        {
+            familyMembers = new List<Person>();
+        }
+
+    }
+}
