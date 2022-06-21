d18e056ab78ae98b124f62816acae42a74950c59
Vencislav <vencislav67@hotmail.com>
2022-06-21 18:17:01 +0300

    Add project files.

---
+++ DefiningClasses/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
