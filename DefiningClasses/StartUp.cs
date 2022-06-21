74b32d5ea894d75c93e4d8d884973d6948c9a29f
Vencislav <vencislav67@hotmail.com>
2022-06-21 19:03:15 +0300

    3

--- DefiningClasses/StartUp.cs
+++ DefiningClasses/StartUp.cs
@@ -11,6 +11,23 @@ namespace DefiningClasses
             person1.Age = 20;
             Person person2 = new Person("George", 18);
             Person person3 = new Person("Jose", 43);
+
+            Family family = new Family();
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person();
+                person.Name = s[0];
+                person.Age = int.Parse(s[1]);
+                family.AddMember(person);
+            }
+
+            Person oldestPerson = new Person();
+            oldestPerson = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
         }
     }
 }
