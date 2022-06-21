effb6a7335fd858d10fb2f2df87371dffd6e91cd
Vencislav <vencislav67@hotmail.com>
2022-06-21 20:19:24 +0300

    5

---
+++ DefiningClasses/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    internal class DateModifier
+    {
+        private int year;
+        private int month;
+        private int day;
+
+        public DateModifier()
+        {
+
+        }
+
+    }
+}
