084ca8f98358a9f1fde7d047e42eddcca796492f
Vencislav <vencislav67@hotmail.com>
2022-06-21 20:40:30 +0300

    5

--- DefiningClasses/DateModifier.cs
+++ DefiningClasses/DateModifier.cs
@@ -27,5 +27,35 @@ namespace DefiningClasses
             get { return year1; }
             set { year1 = int.Parse(date1.Split(' ')[0]); }
         }
+        public int Year2
+        {
+            get { return year2; }
+            set { year2 = int.Parse(date2.Split(' ')[0]); }
+        }
+        public int Month1
+        {
+            get { return month1; }
+            set { month1 = int.Parse(date1.Split(' ')[0]); }
+        }
+        public int Month2
+        {
+            get { return month2; }
+            set { month2 = int.Parse(date2.Split(' ')[0]); }
+        }
+        public int Day1
+        {
+            get { return day1; }
+            set { day1 = int.Parse(date1.Split(' ')[0]); }
+        }
+        public int Day2
+        {
+            get { return day2; }
+            set { day2 = int.Parse(date2.Split(' ')[0]); }
+        }
+
+        public void PrintDifference()
+        {
+            
+        }
     }
 }
